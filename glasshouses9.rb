1ba22b20e5c2ea6b37c4ce80d1c92e99e12183bb
Jacob Armstrong <43141265+jakeparmstrong@users.noreply.github.com>
2019-06-14 10:38:07 -0400

    Update glasshouses9.rb

--- glasshouses9.rb
+++ glasshouses9.rb
@@ -1,34 +1,35 @@
-# Welcome to Sonic Pi v3.1
-
 # Glass Houses - Movement 9
 # Composer: Ann Southam
 # Arranger: Jacob Armstrong
 
-use_bpm 160 #Gives us a tempo of q=96
-use_synth :chiplead
+use_bpm 170
+use_synth :sine
 
-in_thread(name: :righthand) do
-  count2 = 1
-  2.times do
-    4.times do
-      play :e4, pan: 0.8, amp: 0.8
-      sleep 1
-      play :d4, pan: 0.8, amp: 0.8
-      sleep 1
-      play :fs3, pan: 0.8, amp: 0.8
-      sleep 1
-    end
-    4.times do
-      play :e4, pan: 0.8, amp: 0.8
-      sleep 1
-      play :cs4, pan: 0.8, amp: 0.8
-      sleep 1
-      play :a4, pan: 0.8, amp: 0.8
-      sleep 1
-      play :cs4, pan: 0.8, amp: 0.8
-      sleep 1
-    end
+define :first do #3 x 4 = 16
+  4.times do
+    play :e4, pan: 0.8, amp: 0.8
+    sleep 1
+    play :d4, pan: 0.8, amp: 0.8
+    sleep 1
+    play :fs3, pan: 0.8, amp: 0.8
+    sleep 1
   end
+end
+
+define :second do #4 x 4 = 16
+  4.times do
+    play :e4, pan: 0.8, amp: 0.8
+    sleep 1
+    play :cs4, pan: 0.8, amp: 0.8
+    sleep 1
+    play :a4, pan: 0.8, amp: 0.8
+    sleep 1
+    play :cs4, pan: 0.8, amp: 0.8
+    sleep 1
+  end
+end
+
+define :third do #22 x 2 = 44
   2.times do
     play :cs5, pan: 0.8, amp: 0.8
     sleep 1
@@ -78,37 +79,95 @@ in_thread(name: :righthand) do
     sleep 0.5
     play :a4, pan: 0.8, amp: 0.8
     sleep 0.5
-    play :d5, pan: 0.8, amp: 0.8
+    play :b4, pan: 0.8, amp: 0.8
     sleep 0.5
     play :a5, pan: 0.8, amp: 0.8
     sleep 1
   end
-  
-  
-  
+end
+
+define :fourth do #4 x 11 = 44
+  4.times do
+    play :a5, pan: 0.8, amp: 0.8
+    sleep 0.5
+    play :cs5, pan: 0.8, amp: 0.8
+    sleep 0.5
+    play :e5, pan: 0.8, amp: 0.8
+    sleep 0.5
+    play :cs6, pan: 0.8, amp: 0.8
+    sleep 0.5
+    play :e6, pan: 0.8, amp: 0.8
+    sleep 0.5
+    play :a5, pan: 0.8, amp: 0.8
+    sleep 0.5
+    play :cs6, pan: 0.8, amp: 0.8
+    sleep 0.5
+    play :a6, pan: 0.8, amp: 0.8
+    sleep 0.5
+    play :a5, pan: 0.8, amp: 0.8
+    sleep 1
+    play :cs7, pan: 0.8, amp: 0.8
+    sleep 1
+    play :a5, pan: 0.8, amp: 0.8
+    sleep 0.5
+    play :cs5, pan: 0.8, amp: 0.8
+    sleep 0.5
+    play :e5, pan: 0.8, amp: 0.8
+    sleep 0.5
+    play :cs6, pan: 0.8, amp: 0.8
+    sleep 0.5
+    play :e6, pan: 0.8, amp: 0.8
+    sleep 0.5
+    play :a5, pan: 0.8, amp: 0.8
+    sleep 0.5
+    play :cs6, pan: 0.8, amp: 0.8
+    sleep 0.5
+    play :a6, pan: 0.8, amp: 0.8
+    sleep 0.5
+    play :a5, pan: 0.8, amp: 0.8
+    sleep 1
+  end
+end
+
+
+
+in_thread(name: :righthand) do
+  use_synth :chiplead
+  2.times do
+    first #16
+    second #16
+  end
+  third #44
+  2.times do
+    first #16
+    second #16
+  end
+  third #44
+  fourth #44
+  2.times do
+    first #16
+    second #16
+  end
+  third #44
+  fourth #44
 end
 
 in_thread(name: :lefthand) do
-  count2 = 1
-  3.times do
-    count = 1
-    4.times do
-      6.times do
-        play :a2, pan: -0.8, amp: 0.8
-        sleep 0.5
-        play :a3, pan: -0.8, amp: 0.8
-        sleep 0.5
-        play :gs3, pan: -0.8, amp: 0.8
-        sleep 0.5
-        play :b3, pan: -0.8, amp: 0.8
-        sleep 0.5
-        play :d3, pan: -0.8, amp: 0.8
-        sleep 0.5
-        play :a3, pan: -0.8, amp: 0.8
-        sleep 0.5
-        play :e3, pan: -0.8, amp: 0.8
-        sleep 0.5
-      end
-    end
+  use_synth :chipbass
+  120.times do
+    play :a2, pan: -0.8, amp: 0.8
+    sleep 0.5
+    play :a3, pan: -0.8, amp: 0.8
+    sleep 0.5
+    play :gs3, pan: -0.8, amp: 0.8
+    sleep 0.5
+    play :b3, pan: -0.8, amp: 0.8
+    sleep 0.5
+    play :d3, pan: -0.8, amp: 0.8
+    sleep 0.5
+    play :a3, pan: -0.8, amp: 0.8
+    sleep 0.5
+    play :e3, pan: -0.8, amp: 0.8
+    sleep 0.5
   end
 end
